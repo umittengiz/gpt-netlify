cc4d2162fe99f28f49166ecb1dfb9c226354554c
umittengiz <umitengiz@gmail.com>
2023-03-03 20:10:25 +0300

    init

--- express/server.js
+++ express/server.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 router.post('/chat', async (req, res) => {
