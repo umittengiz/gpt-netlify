1b0361fc4379c843d26226a4e30a1f56d3cceda2
umittengiz <umitengiz@gmail.com>
2023-03-03 19:18:38 +0300

    refactor

---
+++ express/server.js
@@ -0,0 +1,45 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+router.post('/chat', async (req, res) => {
+    try {
+        console.log(req.body);
+        const { messages } = req.body;
+        const completion = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages,
+        });
+        const response = completion.data.choices[0].message;
+        res.json({ response });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
