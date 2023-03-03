07f8bb9a50d91883666c81cd16aca4b85fd95a05
umittengiz <umitengiz@gmail.com>
2023-03-03 17:55:31 +0300

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post('/chat', async (req, res) => {
+  try {
+    console.log(req.body);
+    const { messages } = req.body;
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages,
+    });
+    const response = completion.data.choices[0].message;
+    res.json({ response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
